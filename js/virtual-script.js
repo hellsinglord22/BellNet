8b65986392aaf4fd9d384dc835f97733951c387b
H (إتش ) <heshamscorpe22@gmail.com>
2016-03-27 15:57:30 +0000

    Add listener to 3 buttons

--- js/virtual-script.js
+++ js/virtual-script.js
@@ -1,12 +1,27 @@
 // Attributes 
-var EARSER = "eraserTool"; 
+var ERASER = "eraserTool"; 
 var TYPE = "typeTool"; 
 var BRUSH = "brushTool"; 
 
-var currentTool = EARSER; 
+var currentTool = ERASER; 
 
 
 
 $("document").ready(function(){
     
+    // Add listeners 
+    $("#eraserTool").click(function(){
+       alert("I like it when you touch me there");
+       currentTool = ERASER; 
+    });
+    
+     $("#typeTool").click(function(){
+       alert("I like it when you touch me there");
+       currentTool = TYPE; 
+    });
+    
+     $("#brushTool").click(function(){
+       alert("I like it when you touch me there");
+       currentTool = BRUSH; 
+    });
 }); 
