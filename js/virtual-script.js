7520800865b85beb0754d7505923df51ac60c78d
H (إتش ) <heshamscorpe22@gmail.com>
2016-03-27 15:35:38 +0000

    define some constant

--- js/virtual-script.js
+++ js/virtual-script.js
@@ -1,3 +1,12 @@
+// Attributes 
+var EARSER = "eraserTool"; 
+var TYPE = "typeTool"; 
+var BRUSH = "brushTool"; 
+
+var currentTool = EARSER; 
+
+
+
 $("document").ready(function(){
-    alert("This is working just fine"); 
+    
 }); 
