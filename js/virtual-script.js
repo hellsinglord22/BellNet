b2b1dc215d2f920248ad9f790f1fde33192a4faf
H (إتش ) <heshamscorpe22@gmail.com>
2016-04-06 15:32:35 +0000

    final version Type, erase and brush tool

--- js/virtual-script.js
+++ js/virtual-script.js
@@ -12,11 +12,43 @@ var whiteBoard = new fabric.Canvas("whiteBoard");
 
 function brushTool()
 {
+    whiteBoard.freeDrawingBrush.color = 'black';
+    whiteBoard.freeDrawingBrush.width = 10;
     whiteBoard.isDrawingMode = true; 
 }
 
 function moveTool(){
+    $("#".concat(currentTool)).css("background-color", "#bbb"); 
     whiteBoard.isDrawingMode = false; 
+}
+function clearallTool()
+{   
+    var result = confirm("Want to delete?");
+        if(result){
+            $(".item").css("background-color", "white"); 
+            whiteBoard.clear();
+        }
+    
+    
+}
+function eraserTool() 
+{
+   whiteBoard.isDrawingMode = true;
+   whiteBoard.freeDrawingBrush.width = 50;
+   whiteBoard.freeDrawingBrush.color = 'white';
+}
+
+function typeTool(){
+    whiteBoard.add(new fabric.IText('Tap and Type', { 
+        fontFamily: 'arial black',
+        left: 100, 
+        top: 100 ,
+    }));
+    
+    
+    $(".item").css("background-color", "white");
+    currentTool = 'moveTool';
+    moveTool(); 
 }
 
 
@@ -28,6 +60,7 @@ function moveTool(){
  */
 function changeBehaviour()
 {
+    
     switch (currentTool) {
         case "brushTool":
             brushTool();
@@ -38,12 +71,20 @@ function changeBehaviour()
             break;
             
         case "clearallTool":
-            $(".item").css("background-color", "white"); 
-            whiteBoard.clear();
+            clearallTool();
             break; 
+            
+        case "eraserTool":
+            eraserTool();
+            break;
+            
+        case "typeTool":
+            typeTool(); 
+            break;
+        
         
         default:
-            brushTool();
+            
     }
 }
 
