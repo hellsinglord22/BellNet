3c8d2a53e8fdf1e1cb4f06194f432ae04fb31de7
H <heshamscorpe55@gmail>
2016-04-28 17:54:10 +0200

    add socket support to the server side

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 /*jshint esnext: true*/
 
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app).listen(3000);
+var io = require('socket.io')(server);
+
 
 app.use(function(request, response, next){
     console.log(`${request.method} request for ${request.url}`);
-    next(); 
+    next();
 });
 
 /// server the content of the public directory
 app.use(express.static('public'));
 
-// listen on port 3000
-app.listen(3000);
+
+/// web socket
+io.on('connection', function(clientSocket){
+    clientSocket.emit('message', 'welcome to virtual class room'); 
+});
+
 
 /// log this to the user
 console.log(`app is listeining on port 3000`);
